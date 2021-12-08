d9aeb97085ac093a1179ff305fdb031414bcb17e
Andrew Shuvalov <andrew.shuvalov@mongodb.com>
2021-12-08 23:48:07 +0000

    better formatting

--- src/thread_monitor/thread_monitor.h
+++ src/thread_monitor/thread_monitor.h
@@ -39,131 +39,129 @@ void threadMonitorCheckpoint(uint32_t checkpointId);
 namespace details {
 class ThreadMonitorBase {
 public:
-  struct InternalHistoryRecord {
-    std::atomic<uint32_t> checkpointId;
-    std::atomic<std::chrono::system_clock::duration> durationFromCreation;
+    struct InternalHistoryRecord {
+        std::atomic<uint32_t> checkpointId;
+        std::atomic<std::chrono::system_clock::duration> durationFromCreation;
 
 #ifndef NDEBUG
-    // Sequence number is very expensive to generate and thus
-    // it should be used only with debug builds.
-    std::atomic<uint64_t> sequence;
+        // Sequence number is very expensive to generate and thus
+        // it should be used only with debug builds.
+        std::atomic<uint64_t> sequence;
 #endif
-  };
+    };
 
-  /**
-   * Represents one checkpoint visited by this monitor.
-   */
-  struct HistoryRecord {
-    uint32_t checkpointId;
-    std::chrono::system_clock::time_point timestamp;
+    /**
+     * Represents one checkpoint visited by this monitor.
+     */
+    struct HistoryRecord {
+        uint32_t checkpointId;
+        std::chrono::system_clock::time_point timestamp;
 
 #ifndef NDEBUG
-    // Sequence number is very expensive to generate and thus
-    // it should be used only with debug builds.
-    uint64_t sequence;
+        // Sequence number is very expensive to generate and thus
+        // it should be used only with debug builds.
+        uint64_t sequence;
 #endif
-  };
+    };
 
-  using History = std::vector<HistoryRecord>;
+    using History = std::vector<HistoryRecord>;
 
-  bool isEnabled() const;
+    bool isEnabled() const;
 
-  const std::string &name() const;
+    const std::string& name() const;
 
-  unsigned int depth() const;
+    unsigned int depth() const;
 
-  /**
-   * Returns the snapshot of History, which is the vector of
-   * recently visited checkpoints. The count of checkpoints preserved
-   * in history is parameterized as HistoryDepth below. There is no
-   * performance penalty for keeping longer history, but the summary stack
-   * traces can become unnecessary cluttered.
-   */
-  History getHistory() const;
+    /**
+     * Returns the snapshot of History, which is the vector of
+     * recently visited checkpoints. The count of checkpoints preserved
+     * in history is parameterized as HistoryDepth below. There is no
+     * performance penalty for keeping longer history, but the summary stack
+     * traces can become unnecessary cluttered.
+     */
+    History getHistory() const;
 
-  /**
-   * Returns the timestamp of the last checkpoint visited.
-   */
-  std::chrono::system_clock::time_point lastCheckpointTime() const;
+    /**
+     * Returns the timestamp of the last checkpoint visited.
+     */
+    std::chrono::system_clock::time_point lastCheckpointTime() const;
 
-  static void printHistory(const History &history);
+    static void printHistory(const History& history);
 
 protected:
-  ThreadMonitorBase(std::string name, InternalHistoryRecord *historyPtr,
-                    uint32_t historyDepth, uint32_t firstCheckpointId);
-  // The inheritance is non-virtual as the instance of this class can exist
-  // only on the stack and the destructor by the pointer of the base class
-  // cannot be invoked.
-  ~ThreadMonitorBase();
+    ThreadMonitorBase(std::string name,
+                      InternalHistoryRecord* historyPtr,
+                      uint32_t historyDepth,
+                      uint32_t firstCheckpointId);
+    // The inheritance is non-virtual as the instance of this class can exist
+    // only on the stack and the destructor by the pointer of the base class
+    // cannot be invoked.
+    ~ThreadMonitorBase();
 
-  ThreadMonitorBase(const ThreadMonitorBase &) = delete;
-  ThreadMonitorBase &operator=(const ThreadMonitorBase &) = delete;
+    ThreadMonitorBase(const ThreadMonitorBase&) = delete;
+    ThreadMonitorBase& operator=(const ThreadMonitorBase&) = delete;
 
-  /**
-   * Register a checkpoint with 'id' for this monitor, if enabled.
-   * This is internal implementation to be accessed from
-   * threadMonitorCheckpoint().
-   */
-  void checkpointInternalImpl(uint32_t id);
+    /**
+     * Register a checkpoint with 'id' for this monitor, if enabled.
+     * This is internal implementation to be accessed from
+     * threadMonitorCheckpoint().
+     */
+    void checkpointInternalImpl(uint32_t id);
 
-  void
-  writeCheckpointAtPosition(uint32_t index, uint32_t id,
-                            std::chrono::system_clock::time_point timestamp);
+    void writeCheckpointAtPosition(uint32_t index,
+                                   uint32_t id,
+                                   std::chrono::system_clock::time_point timestamp);
 
-  // We only update the central repository once in a while, for performance.
-  void
-  maybeUpdateCentralRepository(std::chrono::system_clock::time_point timestamp);
+    // We only update the central repository once in a while, for performance.
+    void maybeUpdateCentralRepository(std::chrono::system_clock::time_point timestamp);
 
 private:
-  friend void ::thread_monitor::threadMonitorCheckpoint(uint32_t checkpointId);
+    friend void ::thread_monitor::threadMonitorCheckpoint(uint32_t checkpointId);
 
-  void _maybeRegisterThreadLocal();
+    void _maybeRegisterThreadLocal();
 
-  const std::string _name;
-  InternalHistoryRecord *const _historyPtr;
-  const uint32_t _historyDepth;
+    const std::string _name;
+    InternalHistoryRecord* const _historyPtr;
+    const uint32_t _historyDepth;
 
-  const std::chrono::system_clock::time_point _creationTimestamp =
-      std::chrono::system_clock::now();
-  const std::thread::id _threadId = std::this_thread::get_id();
+    const std::chrono::system_clock::time_point _creationTimestamp =
+        std::chrono::system_clock::now();
+    const std::thread::id _threadId = std::this_thread::get_id();
 
-  // Thread monitor is disabled if there is another instance up the stack.
-  bool _enabled = false;
-  // History is not guarded by a mutex. Instead, the update sequence is:
-  // 1. Advance head if the list is full
-  // 2. Insert new record (possibly where the head was before)
-  // 3. Advance tail
-  // Thus non-atomic value insertion happens outside of head-tail interval.
-  std::atomic<uint32_t> _headHistoryRecord = _historyDepth;
-  std::atomic<uint32_t> _tailHistoryRecord = _historyDepth;
+    // Thread monitor is disabled if there is another instance up the stack.
+    bool _enabled = false;
+    // History is not guarded by a mutex. Instead, the update sequence is:
+    // 1. Advance head if the list is full
+    // 2. Insert new record (possibly where the head was before)
+    // 3. Advance tail
+    // Thus non-atomic value insertion happens outside of head-tail interval.
+    std::atomic<uint32_t> _headHistoryRecord = _historyDepth;
+    std::atomic<uint32_t> _tailHistoryRecord = _historyDepth;
 
-  // Prorate updates to central repository to avoid cache misses.
-  std::chrono::system_clock::time_point _lastCentralRepoUpdateTimestamp =
-    _creationTimestamp;
-  std::chrono::system_clock::duration _centralRepoUpdateInterval;
+    // Prorate updates to central repository to avoid cache misses.
+    std::chrono::system_clock::time_point _lastCentralRepoUpdateTimestamp = _creationTimestamp;
+    std::chrono::system_clock::duration _centralRepoUpdateInterval;
 
-  ThreadMonitorCentralRepository::ThreadRegistration *_registration;
+    ThreadMonitorCentralRepository::ThreadRegistration* _registration;
 
 #ifndef NDEBUG
-  static std::atomic<uint64_t> _globalSequence;
+    static std::atomic<uint64_t> _globalSequence;
 #endif
 };
-} // namespace details
+}  // namespace details
 
 template <uint32_t HistoryDepth = 10>
 class ThreadMonitor : public details::ThreadMonitorBase {
 public:
-  ThreadMonitor(std::string name, uint32_t firstCheckpointId);
+    ThreadMonitor(std::string name, uint32_t firstCheckpointId);
 
 private:
-  // The actual history circular list is stored on stack.
-  InternalHistoryRecord _history[HistoryDepth];
+    // The actual history circular list is stored on stack.
+    InternalHistoryRecord _history[HistoryDepth];
 };
 
 template <uint32_t HistoryDepth>
-ThreadMonitor<HistoryDepth>::ThreadMonitor(std::string name,
-                                           uint32_t firstCheckpointId)
-    : ThreadMonitorBase(std::move(name), _history, HistoryDepth,
-                        firstCheckpointId) {}
+ThreadMonitor<HistoryDepth>::ThreadMonitor(std::string name, uint32_t firstCheckpointId)
+    : ThreadMonitorBase(std::move(name), _history, HistoryDepth, firstCheckpointId) {}
 
-} // namespace thread_monitor
+}  // namespace thread_monitor
--- src/thread_monitor/thread_monitor_central_repository.h
+++ src/thread_monitor/thread_monitor_central_repository.h
@@ -15,166 +15,162 @@ namespace thread_monitor {
 
 namespace details {
 class ThreadMonitorBase;
-} // namespace details
+}  // namespace details
 
 class ThreadMonitorCentralRepository {
 public:
-  static inline constexpr auto kDefaultThreadTimeout = std::chrono::minutes{5};
-  static inline constexpr auto kStaleThreadThreshold =
-      std::chrono::milliseconds{1};
-  // How often the central repository seen alive timestamp is updated.
-  // This is prorated to avoid cache misses.
-  static inline constexpr auto kDefaultReportingInterval =
+    static inline constexpr auto kDefaultThreadTimeout = std::chrono::minutes{5};
+    static inline constexpr auto kStaleThreadThreshold = std::chrono::milliseconds{1};
+    // How often the central repository seen alive timestamp is updated.
+    // This is prorated to avoid cache misses.
+    static inline constexpr auto kDefaultReportingInterval =
 #ifdef NDEBUG
-      std::chrono::seconds{1};  // Production.
+        std::chrono::seconds{1};  // Production.
 #else
-      std::chrono::milliseconds{1};  // Debug can have performance penalty.
+        std::chrono::milliseconds{1};  // Debug can have performance penalty.
 #endif
 
-  struct ThreadRegistration {
-    std::thread::id threadId;
-    // The timestamp updated by the thread itself to indicate
-    // it is alive. For efficiency, this timestamp is not updated
-    // on every checkpoint, but only every few seconds.
-    std::atomic<std::chrono::system_clock::time_point> lastSeenAlive;
-    // Guards 'monitor' pointer protecting from deletion.
-    // This mutex is not used by checkpoints, thus the lock
-    // contention should be extremely low.
-    std::mutex monitorDeletionMutex;
-    // In destructor, the monitor clears this pointer.
-    details::ThreadMonitorBase *monitor;
+    struct ThreadRegistration {
+        std::thread::id threadId;
+        // The timestamp updated by the thread itself to indicate
+        // it is alive. For efficiency, this timestamp is not updated
+        // on every checkpoint, but only every few seconds.
+        std::atomic<std::chrono::system_clock::time_point> lastSeenAlive;
+        // Guards 'monitor' pointer protecting from deletion.
+        // This mutex is not used by checkpoints, thus the lock
+        // contention should be extremely low.
+        std::mutex monitorDeletionMutex;
+        // In destructor, the monitor clears this pointer.
+        details::ThreadMonitorBase* monitor;
 
-    ThreadRegistration(std::thread::id threadId,
-                       details::ThreadMonitorBase *monitor,
-                       std::chrono::system_clock::time_point now)
-        : threadId(threadId), lastSeenAlive(now), monitor(monitor) {}
-  };
+        ThreadRegistration(std::thread::id threadId,
+                           details::ThreadMonitorBase* monitor,
+                           std::chrono::system_clock::time_point now)
+            : threadId(threadId), lastSeenAlive(now), monitor(monitor) {}
+    };
 
-  struct ThreadLivenessState {
-    std::thread::id threadId;
-    std::chrono::system_clock::time_point lastSeenAliveTimestamp;
-  };
+    struct ThreadLivenessState {
+        std::thread::id threadId;
+        std::chrono::system_clock::time_point lastSeenAliveTimestamp;
+    };
 
-  /**
-   * Returns the singleton.
-   */
-  static ThreadMonitorCentralRepository *instance();
+    /**
+     * Returns the singleton.
+     */
+    static ThreadMonitorCentralRepository* instance();
 
-  /**
-   * Sets the internal property to not schedule the monitoring thread for tests.
-   * Returns a dummy boolean to instantiate as a static in tests.
-   */
-  static bool instantiateWithoutMonitorThreadForTests();
+    /**
+     * Sets the internal property to not schedule the monitoring thread for tests.
+     * Returns a dummy boolean to instantiate as a static in tests.
+     */
+    static bool instantiateWithoutMonitorThreadForTests();
 
-  /**
-   * Thread monitors do not update the central repository on every checkpoint,
-   * this is too expensive. Instead, they use this interval to update.
-   */
-  std::chrono::system_clock::duration reportingInterval() const;
+    /**
+     * Thread monitors do not update the central repository on every checkpoint,
+     * this is too expensive. Instead, they use this interval to update.
+     */
+    std::chrono::system_clock::duration reportingInterval() const;
 
-  /**
-   * Changes how often the new thread monitors will need to update the liveness
-   * timestamp. In production, keep the default value, no need to change this.
-   * In integration and stress tests, reduce the interval to spot the lagging
-   * threads more accurately.
-   */
-  void setReportingInterval(std::chrono::system_clock::duration interval);
+    /**
+     * Changes how often the new thread monitors will need to update the liveness
+     * timestamp. In production, keep the default value, no need to change this.
+     * In integration and stress tests, reduce the interval to spot the lagging
+     * threads more accurately.
+     */
+    void setReportingInterval(std::chrono::system_clock::duration interval);
 
-  /**
-   * Changes the deafault interval between monitoring cycles.
-   */
-  void setMonitoringInterval(std::chrono::system_clock::duration interval);
+    /**
+     * Changes the deafault interval between monitoring cycles.
+     */
+    void setMonitoringInterval(std::chrono::system_clock::duration interval);
 
-  /**
-   * Sets how long the thread should be stale before it is considered not live
-   * anymore (frozen, deadlocked), which triggers the fault procedures.
-   */
-  void setThreadTimeout(std::chrono::system_clock::duration timeout);
+    /**
+     * Sets how long the thread should be stale before it is considered not live
+     * anymore (frozen, deadlocked), which triggers the fault procedures.
+     */
+    void setThreadTimeout(std::chrono::system_clock::duration timeout);
 
-  /**
-   * Sets callback to be invoked when a thread liveness error condition is detected.
-   * In production, this callback may be set to terminate the program.
-   */
-  void setLivenessErrorConditionDetectedCallback(std::function<void()> cb);
+    /**
+     * Sets callback to be invoked when a thread liveness error condition is detected.
+     * In production, this callback may be set to terminate the program.
+     */
+    void setLivenessErrorConditionDetectedCallback(std::function<void()> cb);
 
-  /**
-   * Approximate (stale) count of registered threads.
-   * The count should sum several shards, each shard is locked separately.
-   */
-  uint32_t threadCount() const;
+    /**
+     * Approximate (stale) count of registered threads.
+     * The count should sum several shards, each shard is locked separately.
+     */
+    uint32_t threadCount() const;
 
-  /**
-   * Returns the count of how many times the frozen condition was detected.
-   */
-  uint32_t getLivenessErrorConditionDetectedCount() const;
+    /**
+     * Returns the count of how many times the frozen condition was detected.
+     */
+    uint32_t getLivenessErrorConditionDetectedCount() const;
 
-  /**
-   * Returns the snapshot of all instrumented threads with latest
-   * liveness timestamp for every thread. Timestamps are stale to the
-   * interval configured with 'reportingInterval()'.
-   */
-  std::vector<ThreadLivenessState> getAllThreadLivenessStates() const;
+    /**
+     * Returns the snapshot of all instrumented threads with latest
+     * liveness timestamp for every thread. Timestamps are stale to the
+     * interval configured with 'reportingInterval()'.
+     */
+    std::vector<ThreadLivenessState> getAllThreadLivenessStates() const;
 
-  /**
-   * Internal method to register this thread monitor with central repository.
-   * This has to be done from the monitor constructor. There is no de-registration
-   * method. Instead, the monitor clears the pointer to itself and the repository garbage
-   * collects the removed monitors later.
-   */
-  ThreadRegistration *registerThread(std::thread::id threadId,
-                                     details::ThreadMonitorBase *monitor,
-                                     std::chrono::system_clock::time_point now);
+    /**
+     * Internal method to register this thread monitor with central repository.
+     * This has to be done from the monitor constructor. There is no de-registration
+     * method. Instead, the monitor clears the pointer to itself and the repository garbage
+     * collects the removed monitors later.
+     */
+    ThreadRegistration* registerThread(std::thread::id threadId,
+                                       details::ThreadMonitorBase* monitor,
+                                       std::chrono::system_clock::time_point now);
 
-  /**
-   * Internal method to start a monitor cycle. Can be invoked directly in tests.
-   * In production, this is invoked from the monitor thread.
-   * Returns the count of GC elements.
-   */
-  unsigned int runMonitorCycle();
+    /**
+     * Internal method to start a monitor cycle. Can be invoked directly in tests.
+     * In production, this is invoked from the monitor thread.
+     * Returns the count of GC elements.
+     */
+    unsigned int runMonitorCycle();
 
 protected:
-  ThreadMonitorCentralRepository(bool withMonitorThread = true);
-  ~ThreadMonitorCentralRepository();
+    ThreadMonitorCentralRepository(bool withMonitorThread = true);
+    ~ThreadMonitorCentralRepository();
 
 private:
-  // Lock contention hits harder with lower count.
-  // In the benchmarks, 30 shards is about 30% faster than 20 shards, and
-  // is already in the saturation zone.
-  static inline constexpr int kShards = 30;
+    // Lock contention hits harder with lower count.
+    // In the benchmarks, 30 shards is about 30% faster than 20 shards, and
+    // is already in the saturation zone.
+    static inline constexpr int kShards = 30;
 
-  // Returns 'was deleted'. If deleted, changes iterator in place.
-  // Precondition: must be invoked under lock.
-  bool _maybeGarbageCollectRecord(plf::colony<ThreadRegistration> &collection,
-                                  plf::colony<ThreadRegistration>::iterator &it);
+    // Returns 'was deleted'. If deleted, changes iterator in place.
+    // Precondition: must be invoked under lock.
+    bool _maybeGarbageCollectRecord(plf::colony<ThreadRegistration>& collection,
+                                    plf::colony<ThreadRegistration>::iterator& it);
 
-  void _frozenThreadAction();
+    void _frozenThreadAction();
 
-  static ThreadMonitorCentralRepository *
-      _staticInstance(bool withMonitorThread);
+    static ThreadMonitorCentralRepository* _staticInstance(bool withMonitorThread);
 
-  std::atomic<std::chrono::system_clock::duration> _threadTimeout =
-      std::chrono::duration_cast<std::chrono::system_clock::duration>(
-          kDefaultThreadTimeout);
+    std::atomic<std::chrono::system_clock::duration> _threadTimeout =
+        std::chrono::duration_cast<std::chrono::system_clock::duration>(kDefaultThreadTimeout);
 
-  std::chrono::system_clock::duration _reportingInterval = kDefaultReportingInterval;
+    std::chrono::system_clock::duration _reportingInterval = kDefaultReportingInterval;
 
-  // This is invoked when the thread liveness failure condition is detected.
-  std::function<void()> _frozenConditionCallback;
+    // This is invoked when the thread liveness failure condition is detected.
+    std::function<void()> _frozenConditionCallback;
 
-  std::chrono::system_clock::time_point _lastTimeOfFaultAction =
-      std::chrono::system_clock::now();
+    std::chrono::system_clock::time_point _lastTimeOfFaultAction = std::chrono::system_clock::now();
 
-  std::atomic<bool> _terminating{false};
-  std::unique_ptr<std::thread> _monitorThread;
+    std::atomic<bool> _terminating{false};
+    std::unique_ptr<std::thread> _monitorThread;
 
-  using LockableColony = std::pair<plf::colony<ThreadRegistration>, std::mutex>;
-  // Keeps all thread registrations in the pointer-stable, continuous
-  // collection. A non-locked shard is picked at registration time. Each shard
-  // has its own mutex.
-  std::array<LockableColony, kShards> _registrations;
+    using LockableColony = std::pair<plf::colony<ThreadRegistration>, std::mutex>;
+    // Keeps all thread registrations in the pointer-stable, continuous
+    // collection. A non-locked shard is picked at registration time. Each shard
+    // has its own mutex.
+    std::array<LockableColony, kShards> _registrations;
 
-  // Stats.
-  std::atomic<uint32_t> _frozenConditionsDetected;
+    // Stats.
+    std::atomic<uint32_t> _frozenConditionsDetected;
 };
 
-} // namespace thread_monitor
+}  // namespace thread_monitor
