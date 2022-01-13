aca28b327574043848045b09ae1197513d9fe2a6
Andrew Shuvalov <andrew.shuvalov@mongodb.com>
2022-01-13 18:57:51 +0000

    more benchmarks

--- src/thread_monitor/thread_monitor_bm.cpp
+++ src/thread_monitor/thread_monitor_bm.cpp
@@ -8,57 +8,81 @@ namespace thread_monitor {
 namespace {
 
 static void BM_ConcurrentCreateDelete(benchmark::State& state) {
+    if (state.thread_index == 0) {
+        ThreadMonitorCentralRepository::instance()->runMonitorCycle();
+    }
     for (auto _ : state) {
         ThreadMonitor<> monitor("test", 1);
     }
-    ThreadMonitorCentralRepository::instance()->runMonitorCycle();
 }
 
-BENCHMARK(BM_ConcurrentCreateDelete)->Threads(1)->MinTime(1);
-BENCHMARK(BM_ConcurrentCreateDelete)->Threads(1)->MinTime(5);
-BENCHMARK(BM_ConcurrentCreateDelete)->Threads(4)->MinTime(5);
-BENCHMARK(BM_ConcurrentCreateDelete)->Threads(8)->MinTime(5);
-BENCHMARK(BM_ConcurrentCreateDelete)->Threads(16)->MinTime(5);
-BENCHMARK(BM_ConcurrentCreateDelete)->Threads(32)->MinTime(5);
-BENCHMARK(BM_ConcurrentCreateDelete)->Threads(64)->MinTime(5);
-BENCHMARK(BM_ConcurrentCreateDelete)->Threads(128)->MinTime(5);
-BENCHMARK(BM_ConcurrentCreateDelete)->Threads(1024)->MinTime(5);
+BENCHMARK(BM_ConcurrentCreateDelete)->Threads(1)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_ConcurrentCreateDelete)->Threads(1)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_ConcurrentCreateDelete)->Threads(4)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_ConcurrentCreateDelete)->Threads(8)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_ConcurrentCreateDelete)->Threads(16)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_ConcurrentCreateDelete)->Threads(32)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_ConcurrentCreateDelete)->Threads(64)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_ConcurrentCreateDelete)->Threads(128)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_ConcurrentCreateDelete)->Threads(1024)->MinTime(5)->UseRealTime();
 
 static void BM_Checkpoint(benchmark::State& state) {
-    ThreadMonitorCentralRepository::instance()->runMonitorCycle();
+    if (state.thread_index == 0) {
+        ThreadMonitorCentralRepository::instance()->runMonitorCycle();
+    }
     ThreadMonitor<> monitor("test", 1);
     for (auto _ : state) {
         threadMonitorCheckpoint(2);
     }
 }
 
-BENCHMARK(BM_Checkpoint)->Threads(1)->MinTime(1);
-BENCHMARK(BM_Checkpoint)->Threads(4)->MinTime(1);
-BENCHMARK(BM_Checkpoint)->Threads(8)->MinTime(1);
-BENCHMARK(BM_Checkpoint)->Threads(16)->MinTime(1);
-BENCHMARK(BM_Checkpoint)->Threads(32)->MinTime(1);
-BENCHMARK(BM_Checkpoint)->Threads(64)->MinTime(1);
-BENCHMARK(BM_Checkpoint)->Threads(128)->MinTime(1);
-BENCHMARK(BM_Checkpoint)->Threads(1024)->MinTime(1);
+BENCHMARK(BM_Checkpoint)->Threads(1)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_Checkpoint)->Threads(4)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_Checkpoint)->Threads(8)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_Checkpoint)->Threads(16)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_Checkpoint)->Threads(32)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_Checkpoint)->Threads(64)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_Checkpoint)->Threads(128)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_Checkpoint)->Threads(1024)->MinTime(1)->UseRealTime();
 
-static void BM_FullCycle1000Checkpoints(benchmark::State& state) {
+static void BM_FullCycle(benchmark::State& state) {
+    if (state.thread_index == 0) {
+        ThreadMonitorCentralRepository::instance()->runMonitorCycle();
+    }
     for (auto _ : state) {
         ThreadMonitor<> monitor("test", 1);
-        for (int i = 0; i < 1000; ++i) {
+        for (int i = 0; i < state.range(0); ++i) {
             threadMonitorCheckpoint(2);
         }
     }
-    ThreadMonitorCentralRepository::instance()->runMonitorCycle();
 }
 
-BENCHMARK(BM_FullCycle1000Checkpoints)->Threads(1)->MinTime(5);
-BENCHMARK(BM_FullCycle1000Checkpoints)->Threads(4)->MinTime(5);
-BENCHMARK(BM_FullCycle1000Checkpoints)->Threads(8)->MinTime(5);
-BENCHMARK(BM_FullCycle1000Checkpoints)->Threads(16)->MinTime(5);
-BENCHMARK(BM_FullCycle1000Checkpoints)->Threads(32)->MinTime(5);
-BENCHMARK(BM_FullCycle1000Checkpoints)->Threads(64)->MinTime(5);
-BENCHMARK(BM_FullCycle1000Checkpoints)->Threads(128)->MinTime(5);
-BENCHMARK(BM_FullCycle1000Checkpoints)->Threads(1024)->MinTime(5);
+BENCHMARK(BM_FullCycle)->Args({100})->Threads(1)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({100})->Threads(4)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({100})->Threads(8)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({100})->Threads(16)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({100})->Threads(32)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({100})->Threads(64)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({100})->Threads(128)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({100})->Threads(1024)->MinTime(5)->UseRealTime();
+
+BENCHMARK(BM_FullCycle)->Args({1000})->Threads(1)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({1000})->Threads(4)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({1000})->Threads(8)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({1000})->Threads(16)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({1000})->Threads(32)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({1000})->Threads(64)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({1000})->Threads(128)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({1000})->Threads(1024)->MinTime(5)->UseRealTime();
+
+BENCHMARK(BM_FullCycle)->Args({10000})->Threads(1)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({10000})->Threads(4)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({10000})->Threads(8)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({10000})->Threads(16)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({10000})->Threads(32)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({10000})->Threads(64)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({10000})->Threads(128)->MinTime(5)->UseRealTime();
+BENCHMARK(BM_FullCycle)->Args({10000})->Threads(1024)->MinTime(5)->UseRealTime();
 
 static void BM_GCAndMonitor(benchmark::State& state) {
     ThreadMonitorCentralRepository::instance()->runMonitorCycle();
@@ -68,14 +92,14 @@ static void BM_GCAndMonitor(benchmark::State& state) {
     }
 }
 
-BENCHMARK(BM_GCAndMonitor)->Threads(1)->MinTime(1);
-BENCHMARK(BM_GCAndMonitor)->Threads(8)->MinTime(1);
-BENCHMARK(BM_GCAndMonitor)->Threads(32)->MinTime(1);
-BENCHMARK(BM_GCAndMonitor)->Threads(64)->MinTime(1);
-BENCHMARK(BM_GCAndMonitor)->Threads(128)->MinTime(1);
-BENCHMARK(BM_GCAndMonitor)->Threads(256)->MinTime(1);
-BENCHMARK(BM_GCAndMonitor)->Threads(512)->MinTime(1);
-BENCHMARK(BM_GCAndMonitor)->Threads(1024)->MinTime(1);
+BENCHMARK(BM_GCAndMonitor)->Threads(1)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_GCAndMonitor)->Threads(8)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_GCAndMonitor)->Threads(32)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_GCAndMonitor)->Threads(64)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_GCAndMonitor)->Threads(128)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_GCAndMonitor)->Threads(256)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_GCAndMonitor)->Threads(512)->MinTime(1)->UseRealTime();
+BENCHMARK(BM_GCAndMonitor)->Threads(1024)->MinTime(1)->UseRealTime();
 
 }  // namespace
 }  // namespace thread_monitor
